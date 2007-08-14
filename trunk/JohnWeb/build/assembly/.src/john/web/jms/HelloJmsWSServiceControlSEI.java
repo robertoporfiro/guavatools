8625edf2788f963108e9dd673a2257efe5785ff3
john.denny <john.denny@c8c46326-1c37-0410-af75-377fab155b91>
2007-08-14 14:50:27 +0000

    Removed file/folder

--- trunk/JohnWeb/build/assembly/.src/john/web/jms/HelloJmsWSServiceControlSEI.java
+++
@@ -1,8 +0,0 @@
-package john.web.jms;
-
-public interface HelloJmsWSServiceControlSEI extends java.rmi.Remote
-{
-
-    public java.lang.String hello(java.lang.String name) throws java.rmi.RemoteException;
-
-}
