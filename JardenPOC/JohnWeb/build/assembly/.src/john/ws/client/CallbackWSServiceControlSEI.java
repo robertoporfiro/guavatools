2d8fdac240e7f03aa224866b15c846c4a8932859
john.denny <john.denny@c8c46326-1c37-0410-af75-377fab155b91>
2007-08-29 16:45:18 +0000

    Removed file/folder

--- JardenPOC/JohnWeb/build/assembly/.src/john/ws/client/CallbackWSServiceControlSEI.java
+++
@@ -1,8 +0,0 @@
-package john.ws.client;
-
-public interface CallbackWSServiceControlSEI extends java.rmi.Remote
-{
-
-    public void delayedHello(java.lang.String name,int delay) throws java.rmi.RemoteException;
-
-}
