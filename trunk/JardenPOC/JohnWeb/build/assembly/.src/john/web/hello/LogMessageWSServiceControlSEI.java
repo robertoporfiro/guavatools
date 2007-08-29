add771169c0348a21c9ad13ee4e00c3a6e17130e
john.denny <john.denny@c8c46326-1c37-0410-af75-377fab155b91>
2007-08-29 16:45:18 +0000

    Removed file/folder

--- trunk/JardenPOC/JohnWeb/build/assembly/.src/john/web/hello/LogMessageWSServiceControlSEI.java
+++
@@ -1,8 +0,0 @@
-package john.web.hello;
-
-public interface LogMessageWSServiceControlSEI extends java.rmi.Remote
-{
-
-    public void logMessage(java.lang.String message) throws java.rmi.RemoteException;
-
-}
--- trunk/JardenPOC/JohnWeb/build/assembly/.src/john/ws/client/BankWSServiceControlSEI.java
+++
@@ -1,8 +0,0 @@
-package john.ws.client;
-
-public interface BankWSServiceControlSEI extends java.rmi.Remote
-{
-
-    public void debit(java.lang.String account,double amount) throws java.rmi.RemoteException;
-
-}
