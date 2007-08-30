f12b3a610d83937d3b4c5fa469f8153427cb7d2a
peetdenny <peetdenny@c8c46326-1c37-0410-af75-377fab155b91>
2007-08-30 14:36:35 +0000

    First working version of TestHSQLDB.java

--- guavatools/src/jarden/demo/db/TestHSQLDB.java
+++ guavatools/src/jarden/demo/db/TestHSQLDB.java
@@ -2,10 +2,13 @@ package jarden.demo.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class TestHSQLDB {
     Connection c;
+
     public static void main(String[] args) throws SQLException {
         TestHSQLDB test = new TestHSQLDB();
         test.launch();
@@ -13,20 +16,35 @@ public class TestHSQLDB {
 
     private void launch() throws SQLException {
         try {
-            Class.forName("org.hsqldb.jdbcDriver" );
+            Class.forName("org.hsqldb.jdbcDriver");
+            c = DriverManager.getConnection("jdbc:hsqldb:file:data/db.data", "sa", "");
+            System.out.println(c);
+            PreparedStatement ps = c.prepareStatement("select * from PUBLIC.speaker");
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                System.out.println(rs.getString("name")+" "+rs.getString("surname"));
+            }
         } catch (Exception e) {
             System.out.println("ERROR: failed to load HSQLDB JDBC driver.");
             e.printStackTrace();
-            return;
         }
-       c = DriverManager.getConnection("jdbc:hsqldb:file:data/db.data", "sa", "");
-       System.out.println(c);
-       try {
-        Thread.sleep(3600000);
-    } catch (InterruptedException e) {
-     
-    }
-       c.close();
+        finally{
+            issueShutdownCommand();
+            c.close();
+        }
+       
         
+//        try {
+//            Thread.sleep(3600000);
+//        } catch (InterruptedException e) {
+//
+//        }
+        
+
+    }
+    
+    private void issueShutdownCommand() throws SQLException{
+        PreparedStatement ps = c.prepareStatement("SHUTDOWN");
+        ps.execute();
     }
 }
