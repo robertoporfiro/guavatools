25ce26ce68c3d4785e49e2ef13e6309679a5c5d2
peetdenny@gmail.com <peetdenny@gmail.com@c8c46326-1c37-0410-af75-377fab155b91>
2011-02-28 14:03:34 +0000

    Initial Import

---
+++ BergenBankingApp/src/test/banking/AccountServiceInMemImpl.java
@@ -0,0 +1,5 @@
+package test.banking;
+
+public class AccountServiceInMemImpl {
+
+}
---
+++ BergenBankingApp/test_src/test/banking/TestAccountService.java
@@ -0,0 +1,12 @@
+package test.banking;
+
+import org.junit.Test;
+
+
+public class TestAccountService {
+
+	@Test
+	public void testCreateAccount(){
+//		AccountService accountService = new AccountServiceInMemImpl();
+	}
+}
