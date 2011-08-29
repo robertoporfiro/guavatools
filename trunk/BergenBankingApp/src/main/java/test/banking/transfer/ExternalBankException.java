cf9d216bb66be12e923b0aa6bf8fe5f041393d7f
peetdenny@gmail.com <peetdenny@gmail.com@c8c46326-1c37-0410-af75-377fab155b91>
2011-08-29 11:29:15 +0000

    Added mockito

--- trunk/BergenBankingApp/src/main/java/test/banking/transfer/ExternalBankException.java
+++ trunk/BergenBankingApp/src/main/java/test/banking/transfer/ExternalBankException.java
@@ -2,4 +2,9 @@ package test.banking.transfer;
 
 public class ExternalBankException extends Exception {
 
+
+	public ExternalBankException(String string) {
+		super(string);
+	}
+
 }
--- trunk/BergenBankingApp/src/main/java/test/banking/transfer/TransferService.java
+++ trunk/BergenBankingApp/src/main/java/test/banking/transfer/TransferService.java
@@ -1,5 +1,9 @@
 package test.banking.transfer;
 
+import test.banking.transfer.ExternalBank;
+import test.banking.transfer.ExternalBankException;
+import test.banking.transfer.InterbankService;
+
 /**
  * Implement this using mockito
  * @author denny
@@ -11,6 +15,14 @@ public class TransferService {
 
 	public TransferService(InterbankService interbankService2) {
 		this.interbankService = interbankService2;
+	}
+
+	public ExternalBank getExternalBank(int sortcode) throws ExternalBankException {
+		ExternalBank bank = interbankService.getBank(sortcode);
+		if(bank == null){
+			throw new ExternalBankException("Unable to find bank with sort code: "+sortcode);
+		}
+		return bank;
 	}
 
 
